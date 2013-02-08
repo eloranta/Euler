a4c21f04c15a4c2fb6bf4bd8d1b0b53ad58279a8
Hannu Eloranta <hannu.eloranta@f-secure.com>
2013-02-08 13:54:11 +0200

    p1 and p2 again

--- clojure/src/clojure/euler.clj
+++ clojure/src/clojure/euler.clj
@@ -1,1 +1,12 @@
-(range 100)
+(def problem1
+  (reduce + (filter (fn [n] (or (zero? (mod n 3)) (zero? (mod n 5)))) (range 1000))))
+
+(assert (= problem1 233168))
+
+(def fibonacci
+  (map first (iterate (fn [[a b]] [b (+ a b)]) [0 1])))
+
+(def problem2
+  (reduce + (filter even? (take-while (fn [n] (< n 4000000)) fibonacci))))
+
+(assert (= problem2 4613732))
