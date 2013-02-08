5171c714ab0fc47eab8089ca04358ef633b2f43c
Hannu Eloranta <hannu.eloranta@f-secure.com>
2013-02-08 14:53:24 +0200

    started p3

--- clojure/src/clojure/euler.clj
+++ clojure/src/clojure/euler.clj
@@ -10,3 +10,30 @@
   (reduce + (filter even? (take-while (fn [n] (< n 4000000)) fibonacci))))
 
 (assert (= problem2 4613732))
+
+(defn prime_internal? [n]
+  (take-while (fn [i] (<= (* i i) n)) (interleave (map dec (range 6 100 6)) (map inc (range 6 100 6)))))
+
+(defn prime? [n]
+  (cond
+    (< n 2) false
+    (= n 2) true
+    (= n 3) true
+    (zero? (mod n 2)) false
+    (zero? (mod n 3)) false
+    (prime_internal? n) true
+    :else false))
+
+(assert (= (prime? -1) false))
+(assert (= (prime? 0) false))
+(assert (= (prime? 1) false))
+(assert (= (prime? 2) true))
+(assert (= (prime? 3) true))
+(assert (= (prime? 4) false))
+(assert (= (prime? 5) true))
+(assert (= (prime? 6) false))
+(assert (= (prime? 7) true))
+;(assert (= (prime? 25) false))
+
+ 
+(prime_internal? 100)
