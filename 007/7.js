43d9e658c0212a67e21ba454cb0f8f4fbbf87d4b
Hannu Eloranta <hannu.eloranta@f-secure.com>
2013-04-17 15:50:15 +0300

    007 using isPrime() from common

--- 007/7.js
+++ 007/7.js
@@ -1,26 +1,9 @@
+var common = require("../common/common");
+
 var primes = [];
 
 for (var n = 2; n < 200000; ++n)
-	if (isPrime(n))
+	if (common.isPrime(n))
 		primes.push(n);
 
-function isPrime(n){
-	if (n < 2)
-		return false;
-	if (n == 2 || n == 3)
-		return true;
-	var i = 2;
-	while(i*i <= n){
-		if (n % i == 0)
-			return false;
-		i++;
-	}
-	return true;
-}
-
 console.log(primes[10000]);
-
-// var util = require("util");
-
-// for (var i = 0; i < 100; ++i)
-	// console.log(util.format("%d: %d", i, isPrime(i)));
