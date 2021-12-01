ac866a1ccbbad509f81f084de571d656ede2eebc
hannu.eloranta <oh2lhe@gmail.com>
2021-12-01 07:44:36 +0200

    refactor

--- python/Problem60/main.py
+++ python/Problem60/main.py
@@ -2,11 +2,24 @@ def generate_primes(limit):
     primes = [True] * limit
     primes[0] = False
     primes[1] = False
-    for n, is_prime in enumerate(primes):
-        if is_prime:
+    for n, is_prime_number in enumerate(primes):
+        if is_prime_number:
             yield n
             for i in range(n * n, limit, n):
                 primes[i] = False
+
+
+def create_primes(limit):
+    primes = []
+    p = [True] * limit
+    p[0] = False
+    p[1] = False
+    for n, is_prime_number in enumerate(p):
+        if is_prime_number:
+            primes.append(n)
+            for i in range(n * n, limit, n):
+                p[i] = False
+    return primes
 
 
 def is_prime(n):
@@ -31,10 +44,12 @@ def is_prime(n):
     return True
 
 
+def is_valid(a, b):
+    return is_prime(int(str(a) + str(b))) and is_prime(int(str(b) + str(a)))
+
+
 def example_60():
-    primes = []
-    for p in generate_primes(10000):
-        primes.append(p)
+    primes = create_primes(10000)
     primes.remove(2)
     primes.remove(5)
 
@@ -42,35 +57,25 @@ def example_60():
         for j in range(len(primes)):
             if j <= i:
                 continue
-            if is_prime(int(str(primes[i]) + str(primes[j]))) and \
-                    is_prime(int(str(primes[j]) + str(primes[i]))):
+            if is_valid(primes[i], primes[j]):
                 for k in range(len(primes)):
                     if k <= j:
                         continue
-                    if is_prime(int(str(primes[i]) + str(primes[k]))) and \
-                            is_prime(int(str(primes[k]) + str(primes[i]))) and \
-                            is_prime(int(str(primes[j]) + str(primes[k]))) and \
-                            is_prime(int(str(primes[k]) + str(primes[j]))):
+                    if is_valid(primes[i], primes[k]) and\
+                       is_valid(primes[j],primes[k]):
                         for l in range(len(primes)):
                             if l <= k:
                                 continue
-                            if is_prime(int(str(primes[i]) + str(primes[l]))) and \
-                                    is_prime(int(str(primes[l]) + str(primes[i]))) and \
-                                    is_prime(int(str(primes[j]) + str(primes[l]))) and \
-                                    is_prime(int(str(primes[l]) + str(primes[j]))) and \
-                                    is_prime(int(str(primes[k]) + str(primes[l]))) and \
-                                    is_prime(int(str(primes[l]) + str(primes[k]))):
+                            if is_valid(primes[i],primes[l]) and \
+                                    is_valid(primes[j], primes[l]) and \
+                                    is_valid(primes[k], primes[l]):
                                 for m in range(len(primes)):
                                     if m <= l:
                                         continue
-                                    if is_prime(int(str(primes[i]) + str(primes[m]))) and \
-                                            is_prime(int(str(primes[m]) + str(primes[i]))) and \
-                                            is_prime(int(str(primes[j]) + str(primes[m]))) and \
-                                            is_prime(int(str(primes[m]) + str(primes[j]))) and \
-                                            is_prime(int(str(primes[k]) + str(primes[m]))) and \
-                                            is_prime(int(str(primes[m]) + str(primes[k]))) and \
-                                            is_prime(int(str(primes[l]) + str(primes[m]))) and \
-                                            is_prime(int(str(primes[m]) + str(primes[l]))):
+                                    if is_valid(primes[i], primes[m]) and \
+                                            is_valid(primes[j], primes[m]) and \
+                                            is_valid(primes[k], primes[m]) and \
+                                            is_valid(primes[l], primes[m]):
                                         print(primes[i], primes[j], primes[k], primes[l], primes[m])
                                         print(primes[i] + primes[j] + primes[k] + primes[l] + primes[m])
                                         return
